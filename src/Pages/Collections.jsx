a3d4c66f83742a2b00710ea796ad13081c0997e6
hamzic2019 <hamzicharis96@gmail.com>
2022-12-28 03:49:49 +0100

    some changes

--- src/Pages/Collections.jsx
+++ src/Pages/Collections.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Collections.css'
+import { FcDiploma2, FcPlus } from "react-icons/fc";
 
 const Collections = () => {
   return (
@@ -7,7 +8,18 @@ const Collections = () => {
         <div className='title'><h1>Collections</h1></div>
         <br />
         <div className='collection-divs'>
-                <div className="box"></div>
+                <div className="box">
+                    <div >
+                        <FcDiploma2 className='box-logo'/>
+                    </div>
+                    <div>
+                        <h1>School</h1>
+                    </div>
+                </div>
+
+                <div className="placeholder">
+                        <FcPlus className='placeholder-icon' />
+                </div>
 
         </div>
     </div>
